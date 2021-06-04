4bfe058e9a518c1220b0124a29dae11673bf91f3
wajid Minhas <shanitent667@gmail.com>
2021-06-04 12:30:11 +0500

    address Room.jsx
    url addedd

--- src/Room.jsx
+++ src/Room.jsx
@@ -3,10 +3,15 @@ import React, { useState } from 'react';
 function Room() {
 
     const [isLit, setLit] = useState(true);
+    const [age, setAge] = useState();
+
     return (
         <>
             <p>The room is {isLit? "Lit" : "Dark"}.</p>
+            <br/>
+            age : {age}
             <button onClick={() => setLit(!isLit)}>Click Me</button>
+            <button onClick={() => { setAge(++age)}}>Age </button>
         </>
 
     )
